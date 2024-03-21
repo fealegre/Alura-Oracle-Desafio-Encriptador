d06a1711bef5d8f53da3553cf4007b848d9c8096
fealegre <fealegre@hotmail.com>
2024-03-20 21:54:51 -0300

    Refactorización usando funciones de JS. Código mas reutilizable y corto

--- scripts.js
+++ scripts.js
@@ -5,18 +5,17 @@ function limpiar() {
 function transcode(encrypt) {
     let mensaje = document.querySelector("textarea").value;
     let codigo = document.querySelector("p");
-    if (!encrypt){    
-        mensaje = mensaje.replaceAll("enter", "e");
-        mensaje = mensaje.replaceAll("imes", "i");
-        mensaje = mensaje.replaceAll("ai", "a");
-        mensaje = mensaje.replaceAll("ober", "o");
-        mensaje = mensaje.replaceAll("ufat", "u");
-    } else {
-        mensaje = mensaje.replaceAll("e", "enter");
-        mensaje = mensaje.replaceAll("i", "imes");
-        mensaje = mensaje.replaceAll("a", "ai");
-        mensaje = mensaje.replaceAll("o", "ober");
-        mensaje = mensaje.replaceAll("u", "ufat");
-    }
-    codigo.innerText = mensaje;    
+    const substituciones = { "enter": "e", "imes": "i", "ai": "a", "ober": "o", "ufat": "u" };
+    
+    const revertirSubstituciones = Object.fromEntries(
+        Object.entries(substituciones).map(([key, value]) => [value, key])
+    );
+    
+    const aplicarSubstituciones = (text, map) =>
+        Object.entries(map).reduce(
+            (acc, [key, value]) => acc.replaceAll(key, value),
+            text
+        );
+
+    codigo.innerText = encrypt ? aplicarSubstituciones(mensaje, revertirSubstituciones) : aplicarSubstituciones(mensaje, substituciones);
 }
